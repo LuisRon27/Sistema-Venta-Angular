c147e459eb30aabd8506794b330c2126945a5376
Luis Ron <luisron39@gmail.com>
2023-07-26 15:10:06 -0300

    V 1.0.0 Sistema Venta

--- src/app/Components/layout/Pages/dash-board/dash-board.component.ts
+++ src/app/Components/layout/Pages/dash-board/dash-board.component.ts
@@ -1,10 +1,69 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Chart, registerables } from 'chart.js';
+import { DashBoardService } from 'src/app/Services/dash-board.service';
+Chart.register(...registerables)
 
 @Component({
   selector: 'app-dash-board',
   templateUrl: './dash-board.component.html',
   styleUrls: ['./dash-board.component.css']
 })
-export class DashBoardComponent {
+export class DashBoardComponent implements OnInit {
+
+  totalIngresos: string = "0";
+  totalVentas: string = "0";
+  totalProductos: string = "0";
+
+  constructor(
+    private _dashboardServicio: DashBoardService
+  ) { }
+
+
+  mostrarGraficos(labelGrafico: any[], dataGrafico: any[]) {
+    const chartBarras = new Chart('chartBarras', {
+      type: 'bar',
+      data: {
+        labels: labelGrafico,
+        datasets: [{
+          label: '# de Ventas',
+          data: dataGrafico,
+          backgroundColor: ['rgba(54, 162, 235, 0.2)'],
+          borderColor: ['rgba(54, 162, 235, 1)'],
+          borderWidth: 1
+        }]
+      },
+      options: {
+        maintainAspectRatio: false,
+        responsive: true,
+        scales: {
+          y: {
+            beginAtZero: true
+          }
+        }
+      }
+    });
+  }
+
+  ngOnInit(): void {
+    this._dashboardServicio.resumen().subscribe({
+      next: (data) => {
+        if (data.status) {
+          this.totalIngresos = data.value.totalIngresos;
+          this.totalVentas = data.value.totalVenta;
+          this.totalProductos = data.value.totalProductos;
+
+          const arrayData: any[] = data.value.ventasUltimaSemana;
+          console.log(arrayData);
+
+          const labelTemp = arrayData.map((value) => value.fecha);
+          const dataTemp = arrayData.map((value) => value.total);
+
+          this.mostrarGraficos(labelTemp, dataTemp);
+        }
+      },
+      error: (e) => { }
+    })
+  }
+
 
 }
