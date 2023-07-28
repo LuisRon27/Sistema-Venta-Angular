95f82658bd637f22ba3408d93982c009206d1d23
Luis Ron <luisron39@gmail.com>
2023-07-27 22:40:50 -0300

    responsive parte 1

--- src/app/Environments/environment.ts
+++ src/app/Environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  apiEndpoint: "https://localhost:7021/api/"
+  //apiEndpoint: "https://localhost:7021/api/"
+  apiEndpoint: "https://www.apiventa.somee.com/api/"
 };
